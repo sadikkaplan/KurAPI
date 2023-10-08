1a30001acdd91f23dd6c4d9472bd093fbe765406
skaplan <sadokplan.270@gmail.com>
2023-10-08 14:48:46 +0300

    Add project files.

---
+++ KurAPI/Controllers/DailyCurrencyController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace KurAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DailyCurrencyController : ControllerBase
+    {
+        [HttpPost]
+        public ResponseData Run(RequestData data)
+        {
+            ResponseData result = new ResponseData();
+            try
+            {
+                string apiAdress = string.Format("http://www.tcmb.gov.tr/kurlar/{0}.xml", (data.isToday) ? "today" :
+                    string.Format("{2}{1}/{0}{1}{2}", data.Day.ToString().PadLeft(2, '0'), data.Month.ToString().PadLeft(2, '0'), data.Year));
+
+                result.Data = new List<ResponseDataCurrency>();
+
+                XmlDocument doc = new XmlDocument();
+                doc.Load(apiAdress);
+
+                if (doc.SelectNodes("Tarih_Date").Count < 1)
+                {
+                    result.Error = "Kur Bilgisi Okunamadi";
+                    return result;
+                }
+
+                foreach (XmlNode node in doc.SelectNodes("Tarih_Date")[0].ChildNodes)
+                {
+                    ResponseDataCurrency currecy = new ResponseDataCurrency();
+                    
+                    currecy.Code = node.Attributes["Kod"].Value;
+                    currecy.Name = node["Isim"].InnerText;
+                    currecy.Unit = Int32.Parse(node["Unit"].InnerText);
+                    currecy.BuyingRate = Convert.ToDecimal("0" + node["ForexBuying"].InnerText.Replace(".", ","));
+                    currecy.SellingRate = Convert.ToDecimal("0" + node["ForexSelling"].InnerText.Replace(".", ","));
+                    currecy.EffectiveBuyingRate = Convert.ToDecimal("0" + node["BanknoteBuying"].InnerText.Replace(".", ","));
+                    currecy.EffectiveSellingRate = Convert.ToDecimal("0" + node["BanknoteSelling"].InnerText.Replace(".", ","));
+
+                    result.Data.Add(currecy);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                result.Error = ex.Message;
+                //throw;
+            }
+
+
+
+
+            return result;
+        }
+    }
+}
---
+++ KurAPI/Models.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KurAPI
+{
+    public class RequestData
+    {
+        public int Day { get; set; }
+        public int Month { get; set; }
+        public int Year { get; set; }
+        public bool isToday { get; set; }
+    }
+    public class ResponseDataCurrency
+    {
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public int Unit { get; set; }
+        public decimal BuyingRate { get; set; }
+        public decimal SellingRate { get; set; }
+        public decimal EffectiveBuyingRate { get; set; }
+        public decimal EffectiveSellingRate { get; set; }
+    }
+
+    public class ResponseData
+    {
+        public List<ResponseDataCurrency> Data { get; set; }
+        public string Error { get; set; }
+    }
+}
